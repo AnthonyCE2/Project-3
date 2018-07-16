67cb1e152fdb2000d383625b7e0fbc72fdf083fe
cgoodwin1011 <cgoodwin2127@gmail.com>
2018-07-16 18:59:00 -0400

    clean up

--- proj3db 2/X/companyLocHelper.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (companyTools) {
-  companyTools.getInfofromName = (companyName) => {
-    db.companyloc.findAll({
-      where: {
-        companyName: name
-      }
-    }).then(data => {return data})
-  }
-}
