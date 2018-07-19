b29cd36520a28d05b022091103460ee485a367fc
cgoodwin1011 <cgoodwin2127@gmail.com>
2018-07-18 20:14:28 -0400

    database tweak

--- database/seeds.sql
+++ database/seeds.sql
@@ -39,15 +39,3 @@ INSERT INTO companyLoc
 VALUES
   ("Dewey Cheatham & Howe", "Midtown", "1211 Sixth Ave", "", "New York", "NY", "10036");
 
-INSERT INTO posts
-  (userID, companyID, jobID, keepAnonymous, textOfPost, reason)
-VALUES
-  (1, 1, 1, TRUE, "Where do you put the child seat in a Porsche 911?", "Clearly a problem for their execs");
-INSERT INTO postTable
-  (userID, companyID, jobID, keepAnonymous, textOfPost, reason)
-VALUES
-  (2, 3, 2, TRUE, "Bring us money. Tell us how you'll do it.", "At least they're to the point");
-INSERT INTO postTable
-  (userID, companyID, jobID, keepAnonymous, textOfPost, reason)
-VALUES
-  (3, 2, 3, TRUE, "How many marbles could you put in the Grand Canyon?", "Lots, I said");
